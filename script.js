9f4376bbcc6f86e30ec3677d581b0ab77e504ceb
BiellEsdrass <gabriell.com.br13@gmail.com>
2023-01-31 00:22:59 -0300

    calculadora de IMC

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const altura = document.getElementById("altura");
-const peso = document.getElementById("peso");
 
 const btnCalcular = document.getElementById("btn-calcular"); //botão
 
 const formulario = document.querySelector('.form') //formulario
 
 //alert
-let box = document.querySelector('.box')
+const box = document.querySelector('.box')
 let resultado = document.getElementById('resultado');
+
+
+let back = document.getElementById('btn-back');
 
 
 
@@ -16,8 +17,6 @@ function calculoImc(event){
     event.preventDefault();
 
     const resultado = document.getElementById('resultado');
-    const comentario = document.getElementById('comment');
-    const voltar = document.querySelector('.btn-back') ///  COLOCAR UMA FUNÇÃO
 
     let kilos = +formulario.kilos.value.replace(',','.');
     let centimetros = +formulario.centimetros.value / 100 .toFixed(2);
@@ -28,61 +27,65 @@ function calculoImc(event){
     resultado.innerText = imc;
     resultado.style.display='block';
 
-    if (imc < 18.5){
-        comment.style.color='rgb(127, 195, 255)';
-        comment.textContent = "Abaixo do peso";
-        console.log(comment)
-
-        box.style.display='flex';
-        resultado.style.color='rgb(127, 195, 255)';
-    } 
-    
-    else if(imc >= 18.6 && imc <= 24.9 ){
-        comment.style.color='#5cef9c';
-        comment.textContent = "Peso ideal";
-        console.log(comment)
-
-        box.style.display='flex';
-        resultado.style.color='#5cef9c';
+    back.onclick = function(){
+        box.style.display='none'
     }
 
-    else if(imc >= 25 && imc <= 29.9){
-        comment.style.color='#fcff4b';
-        comment.textContent = "Sobrepeso";
-        console.log(comment)
 
-        box.style.display='flex';
-        resultado.style.color='#fcff4b';
-    }
-
-    else if(imc >= 30 && imc <= 34.9){
-        comment.style.color='#cd9b36';
-        comment.textContent = "Obesidade grau 1";
-        console.log(comment)
-
-        box.style.display='flex';
-        resultado.style.color='#cd9b36';
-    }
-
-    else if(imc >= 35 && imc <= 39.9){
-        comment.style.color='#d0661b';
-        comment.textContent = "Obesidade grau 2";
-        console.log(comment)
-
-        box.style.display='flex';
-        resultado.style.color='#d0661b';
-    }
-
-    else if(imc >= 40){
-        comment.style.color='#d01b1b';
-        comment.textContent = "Obesidade grau 2";
-        console.log(comment)
-
-        box.style.display='flex';
-        resultado.style.color='#d01b1b';
-    }
-}
-
+            if (imc < 18.5){
+                comment.style.color='rgb(127, 195, 255)';
+                comment.textContent = "Abaixo do peso";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='rgb(127, 195, 255)';
+            } 
+            
+            else if(imc >= 18.6 && imc <= 24.9 ){
+                comment.style.color='#5cef9c';
+                comment.textContent = "Peso ideal";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='#5cef9c';
+            }
+        
+            else if(imc >= 25 && imc <= 29.9){
+                comment.style.color='#fcff4b';
+                comment.textContent = "Sobrepeso";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='#fcff4b';
+            }
+        
+            else if(imc >= 30 && imc <= 34.9){
+                comment.style.color='#cd9b36';
+                comment.textContent = "Obesidade grau 1";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='#cd9b36';
+            }
+        
+            else if(imc >= 35 && imc <= 39.9){
+                comment.style.color='#d0661b';
+                comment.textContent = "Obesidade grau 2";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='#d0661b';
+            }
+        
+            else if(imc >= 40){
+                comment.style.color='#d01b1b';
+                comment.textContent = "Obesidade grau 2";
+                console.log(comment)
+        
+                box.style.display='flex';
+                resultado.style.color='#d01b1b';
+            }
+        }
 
 
 btnCalcular.addEventListener('click', calculoImc)
